7b300b7726b2e4c83006320e40cfd6f6f8fb9ed6
Julien Malard <julien.malard@mail.mcgill.ca>
2016-02-05 10:05:00 -0500

    Lo estoy guardando mientras que todavía funciona.

--- Prueba.py
+++ Prueba.py
@@ -5,75 +5,97 @@ import struct
 # necesario para el funcionamiento del programa Tinamit.
 
 
-def verificar_vensim():
-    estatus = int(dll.vensim_check_status())
-    if estatus == 0:
-        print('Listo.', estatus)
-    elif estatus == 1:
-        print('Simulando.', estatus)
-    elif estatus == 5:
-        print('Jugando.', estatus)
-    elif estatus == 6:
-        print('Memoria no libre.', estatus)
-    else:
-        print('Tienes un problema.', estatus)
+class Dll(object):
+    def __init__(símismo, ubic_modelo):
+        símismo.variables = None
 
-dll = ctypes.WinDLL('C:\\Windows\\System32\\vendll32.dll')
-print('1')
-verificar_vensim()
+        símismo.dll = ctypes.WinDLL('C:\\Windows\\System32\\vendll32.dll')
+        resultado = símismo.dll.vensim_command(('SPECIAL>LOADMODEL|"%s"' % ubic_modelo).encode())
+        print('Cargando... ', resultado)
+        símismo.verificar_estatus()
+
+    def sacar_nombres_vars(símismo):
+        mem_inter = ctypes.create_string_buffer(100)
+        resultado = símismo.dll.vensim_get_varnames(b"*", 0, mem_inter, 100)
+        símismo.variables = [x for x in mem_inter.raw.decode().split('\x00') if x]
+
+        print('Sacando variables... ', resultado)
+        símismo.verificar_estatus()
+
+    def estab_nombre_corrida(símismo, nombre):
+        resultado = símismo.dll.vensim_command(("SIMULATE>RUNNAME|%s" % nombre).encode())
+        print('Estableciendo nombre de corrida... ', resultado)
+        símismo.verificar_estatus()
+
+    def empezar_simul(símismo):
+        resultado = símismo.dll.vensim_start_simulation(1, 2, 1)
+        print('Empezando simulación... ', resultado)
+        símismo.verificar_estatus()
+
+    def avanzar_simul(símismo, paso):
+        resultado = símismo.dll.vensim_continue_simulation(paso)
+        print('Avanzando simulación... ', resultado)
+        símismo.verificar_estatus()
+
+    def empezar_juego(símismo):
+        resultado = símismo.dll.vensim_command("MENU>GAME")
+        print('Empezando juego... ', resultado)
+        símismo.verificar_estatus()
+
+    def estab_paso_juego(símismo, paso):
+        resultado = símismo.dll.vensim_command(("GAME>GAMEINTERVAL|%i" % paso).encode())
+        print('Estableciendo paso juego... ', resultado)
+        símismo.verificar_estatus()
+
+    def avanzar_juego(símismo):
+        resultado = símismo.dll.vensim_command(b"GAME>GAMEON")
+        print('Avanzando juego... ', resultado)
+        símismo.verificar_estatus()
+
+    def terminar_juego(símismo):
+        resultado = símismo.dll.vensim_command("GAME>ENDGAME")
+        print('Terminando juego... ', resultado)
+        símismo.verificar_estatus()
+
+    def sacar_val_var(símismo, var):
+        mem_inter = ctypes.create_string_buffer(4)
+        resultado = símismo.dll.vensim_get_val(var.encode(), mem_inter)
+        print('Sacando valor var %s... ' % var, resultado)
+        print(var, struct.unpack('f', mem_inter))
+        símismo.verificar_estatus()
+
+    def poner_val_var(símismo, val, var):
+        símismo.dll.vensim_command(('SIMULATE>SETVAL|%s = %i' % (var, val)).encode())
+
+    def terminar_simulación(símismo):
+        resultado = símismo.dll.vensim_finish_simulation()
+        print('Terminando simulación... ', resultado)
+        símismo.verificar_estatus()
+
+    def verificar_estatus(símismo):
+        estatus = int(símismo.dll.vensim_check_status())
+        if estatus == 0:
+            print('Listo.', estatus)
+        elif estatus == 1:
+            print('Simulando.', estatus)
+        elif estatus == 5:
+            print('Jugando.', estatus)
+        elif estatus == 6:
+            print('Memoria no libre.', estatus)
+        else:
+            print('Tienes un problema.', estatus)
+
 ubicación_modelo = "C:\\Users\\jmalar1\\Documents\\PycharmProjects\\Tinamit\\Prueba dll.vpm"
+modelo = Dll(ubicación_modelo)
+modelo.sacar_nombres_vars()
 
-dll.vensim_command('')
-print('2')
-verificar_vensim()
+modelo.estab_nombre_corrida('Corrida')
+modelo.estab_paso_juego(1)
+modelo.empezar_juego()
+modelo.sacar_val_var('FINAL TIME')
+modelo.sacar_val_var('Nivel')
 
-dll.vensim_command(('SPECIAL>LOADMODEL|"%s"' % ubicación_modelo).encode())
-intermed = ctypes.create_string_buffer(100)
-dll.vensim_get_varnames(b"*", 0, intermed, 100)
-print([x for x in intermed.raw.decode().split('\x00') if x])
-
-print('3')
-verificar_vensim()
-
-var = 'FINAL TIME'
-comanda = ('SIMULATE>SETVAL|%s = %i' % (var, 200)).encode()
-dll.vensim_command(comanda)
-
-intermed_0 = ctypes.create_string_buffer(4)
-dll.vensim_get_val(var.encode(), intermed_0)
-print('val : ', struct.unpack('f', intermed_0)[0])
-
-dll.vensim_start_simulation(1, 0, 1)
-verificar_vensim()
-
-comanda = ('SIMULATE>SETVAL|%s = %i' % (var, 300)).encode()
-dll.vensim_command(comanda)
-dll.vensim_get_val(var.encode(), intermed_0)
-print('val : ', struct.unpack('f', intermed_0)[0])
-
-variable = 'Variable 1'
-inter1 = ctypes.create_string_buffer(4)
-
-dll.vensim_get_val(variable.encode(), inter1)
-
-print('Variable inicial:', struct.unpack('f', inter1)[0])
-
-comanda = ('SIMULATE>SETVAL|%s = %i' % (variable, 200)).encode()
-
-dll.vensim_command(comanda)
-
-inter2 = ctypes.create_string_buffer(4)
-
-dll.vensim_get_val(variable.encode(), inter2)
-print('Variable modificado:', struct.unpack('f', inter2)[0])
-
-dll.vensim_continue_simulation(10)
-dll.vensim_get_val(variable.encode(), inter2)
-
-print('Variable modificado:', struct.unpack('f', inter2)[0])
-
-dll.vensim_finish_simulation()
-
-verificar_vensim()
+modelo.avanzar_juego()
+modelo.sacar_val_var('Nivel')
 
 print('fin')
