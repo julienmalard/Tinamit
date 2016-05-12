9659142f2be88407ad004953baf584abd0f18ce3
Julien Malard <julien.malard@mail.mcgill.ca>
2016-05-12 16:52:53 -0400

    Creación de una envolura específica para SAHYSMOD, un modelo de agua subterránea.

--- Ejemplos/Ejemplo SAHYSMOD/Coupling script.py
+++ Ejemplos/Ejemplo SAHYSMOD/Coupling script.py
@@ -10,11 +10,11 @@ modelo.estab_bf('C:\\Users\\jmalar1\\Documents\\PycharmProjects\\Tinamit\\Ejempl
 modelo.actualizar()
 
 # Change variable names as needed
-modelo.conexiones=[{"var_mds": "Groundwater Table", "mds_fuente": False, "conv": 1, "var_bf": "Groundwater Depth"},
-                   {"var_mds": "Groundwater Extraction", "mds_fuente": True, "conv": 1, "var_bf":
-                       "Groundwater Extraction"},
-                   {"var_mds": "Soil Salinity", "mds_fuente": False, "conv": 1, "var_bf": "Soil Salinity"}
-                   ]
+modelo.conexiones = [{"var_mds": "Groundwater Table", "mds_fuente": False, "conv": 1, "var_bf": "Groundwater Depth"},
+                     {"var_mds": "Groundwater Extraction", "mds_fuente": True, "conv": 1, "var_bf":
+                         "Groundwater Extraction"},
+                     {"var_mds": "Soil Salinity", "mds_fuente": False, "conv": 1, "var_bf": "Soil Salinity"}
+                     ]
 
 modelo.actualizar_conexiones()
 
