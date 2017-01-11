0f8050d35c6d40aaace2617fcd89e2ea6b2bb215
Julien Malard <julien.malard@mail.mcgill.ca>
2017-01-11 09:04:50 -0500

    Update Conectado.py

--- tinamit/Conectado.py
+++ tinamit/Conectado.py
@@ -362,12 +362,17 @@ class SuperConectado(Modelo):
             if var not in mod.variables:
                 raise ValueError('El variable "{}" no existe en el modelo "{}".'.format(var, nombre_mod))
 
-            # Y también asegurarse de que el variable no a sido conectado ya.
-            if var in mod.vars_saliendo or var in mod.vars_entrando:
-                raise ValueError('El variable "{}" del modelo "{}" ya está conectado. '
-                                 'Desconéctalo primero con .desconectar_vars().'.format(var, nombre_mod))
+            
 
-        
+        # Identificar el nombre del modelo recipiente también.
+        índ_mod_fuente = l_mods.index(modelo_fuente)
+        modelo_recip = l_mods[(índ_mod_fuente + 1) % 2]
+
+        # Identificar los nombres de los variables fuente y recipiente, tanto como sus unidades.
+        var_fuente = dic_vars[modelo_fuente]
+        var_recip = dic_vars[modelo_recip]
+        unid_fuente = símismo.modelos[modelo_fuente].variables[var_fuente]['unidades']
+        unid_recip = símismo.modelos[modelo_recip].variables[var_recip]['unidades']
         
         
         if conv is None:
