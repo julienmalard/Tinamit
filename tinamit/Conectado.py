68a91f27b9cbdf0a4ab49a00fddf4ad21ba35ed1
Julien Malard <julien.malard@mail.mcgill.ca>
2017-01-11 09:14:51 -0500

    Update Conectado.py

--- tinamit/Conectado.py
+++ tinamit/Conectado.py
@@ -367,15 +367,15 @@ class SuperConectado(Modelo):
                 raise ValueError('El variable "{}" del modelo "{}" ya está conectado. '
                                  'Desconéctalo primero con .desconectar_vars().'.format(var, nombre_mod))
 
-        
+        # Identificar el nombre del modelo recipiente también.
+        índ_mod_fuente = l_mods.index(modelo_fuente)
+        modelo_recip = l_mods[(índ_mod_fuente + 1) % 2]
 
-        # Crear el diccionario de conexión.
-        dic_conex = {'modelo_fuente': modelo_fuente,
-                     'vars': {modelo_recip: var_recip,
-                              modelo_fuente: var_fuente
-                              },
-                     'conv': conv
-                     }
+        # Identificar los nombres de los variables fuente y recipiente, tanto como sus unidades.
+        var_fuente = dic_vars[modelo_fuente]
+        var_recip = dic_vars[modelo_recip]
+        unid_fuente = símismo.modelos[modelo_fuente].variables[var_fuente]['unidades']
+        unid_recip = símismo.modelos[modelo_recip].variables[var_recip]['unidades']
 
         # Agregar el diccionario de conexión a la lista de conexiones.
         símismo.conexiones.append(dic_conex)
