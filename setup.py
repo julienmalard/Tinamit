82e024bce814a03ec69f025824fcce2aa63fed5e
Julien Malard <julien.malard@mail.mcgill.ca>
2017-01-11 09:42:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tinamit',
-    version='1.2.1rcJULIEN.MALARD',
+    version='1.2.1',
     packages=find_packages(),
     url='https://github.com/julienmalard/Tinamit',
     license='GNU GPL 3',
