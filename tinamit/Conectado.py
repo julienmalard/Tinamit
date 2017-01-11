03103ddecf6d252a4f80b9f31919d50fd259d881
Julien Malard <julien.malard@mail.mcgill.ca>
2017-01-11 08:57:55 -0500

    Update Conectado.py

--- tinamit/Conectado.py
+++ tinamit/Conectado.py
@@ -349,7 +349,36 @@ class SuperConectado(Modelo):
     def conectar_vars(símismo, dic_vars, modelo_fuente, conv=None):
 
 
-        pass
+        # Una lista de los submodelos.
+        l_mods = list(símismo.modelos)
+
+        # Asegurarse de que modelos y variables especificados sí existan.
+        for nombre_mod in dic_vars:
+            if nombre_mod not in símismo.modelos:
+                raise ValueError('Nombre de modelo "{}" erróneo.'.format(nombre_mod))
+
+            mod = símismo.modelos[nombre_mod]
+            var = dic_vars[nombre_mod]
+
+            if var not in mod.variables:
+                raise ValueError('El variable "{}" no existe en el modelo "{}".'.format(var, nombre_mod))
+
+            # Y también asegurarse de que el variable no a sido conectado ya.
+            if var in mod.vars_saliendo or var in mod.vars_entrando:
+                raise ValueError('El variable "{}" del modelo "{}" ya está conectado. '
+                                 'Desconéctalo primero con .desconectar_vars().'.format(var, nombre_mod))
+
+        # Identificar el nombre del modelo recipiente también.
+        índ_mod_fuente = l_mods.index(modelo_fuente)
+        modelo_recip = l_mods[(índ_mod_fuente + 1) % 2]
+
+        # Identificar los nombres de los variables fuente y recipiente, tanto como sus unidades.
+        var_fuente = dic_vars[modelo_fuente]
+        var_recip = dic_vars[modelo_recip]
+        unid_fuente = símismo.modelos[modelo_fuente].variables[var_fuente]['unidades']
+        unid_recip = símismo.modelos[modelo_recip].variables[var_recip]['unidades']
+
+        
 
     def desconectar_vars(símismo, var_fuente, modelo_fuente):
         """
