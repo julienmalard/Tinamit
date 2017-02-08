5bd88a243d63acea2611b6e7c79d75dc56c9e52f
Julien Malard <julien.malard@mail.mcgill.ca>
2017-02-08 11:11:11 -0500

    Más trabajo.

--- tinamit/__init__.py
+++ tinamit/__init__.py
@@ -1,3 +1,11 @@
+from pkg_resources import resource_filename
+
 __author__ = 'Julien Malard'
 
 __notes__ = 'Contacto: julien.malard@mail.mcgill.ca'
+
+
+with open(resource_filename('tinamit', 'versión.txt')) as archivo_versión:
+    versión = archivo_versión.read().strip()
+
+__version__ = versión
