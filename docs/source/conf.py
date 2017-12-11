339f623bf54975f78b1e187deb065d8f5bce9961
Julien Malard <julien.malard@mail.mcgill.ca>
2017-12-11 12:18:05 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -142,8 +142,6 @@ todo_include_todos = True
 con_rtd = os.environ.get('READTHEDOCS')
 if con_rtd:
     html_theme = 'default'
-    def setup(app):
-        app.add_stylesheet("tema.css")
 else:
     html_theme = 'alabaster'
 
