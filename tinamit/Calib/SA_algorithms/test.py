f73c7407cfd043bcae75e04fa14f93e9aa49e7a5
GabriellaPeng <xingyu.peng@mail.mcgill.ca>
2018-03-23 15:07:46 -0400

    星谕

---
+++ tinamit/Calib/SA_algorithms/test.py
@@ -0,0 +1,13 @@
+# from tinamit.Calib import read_xl
+# import numpy as np
+# import json
+#
+# a = [[1, 2], [3, 4], [5, 6]]
+# a = np.asarray(a)
+# print(a.transpose())
+# with open('sensitivity.out', 'r+') as f:
+#     s = a.tolist()
+#     for p in s:
+#         print(p)
+#         f.write(json.dumps(p)+'\n')
+# f.close()
