0ebaa3d02858a6de277775cc21f150ceae0f52ea
GabriellaPeng <xingyu.peng@mail.mcgill.ca>
2018-03-23 15:26:24 -0400

    星谕

--- tinamit/Calib/SA_algorithms/test.py
+++ tinamit/Calib/SA_algorithms/test.py
@@ -1,13 +1,20 @@
-# from tinamit.Calib import read_xl
-# import numpy as np
-# import json
+from tinamit.EnvolturaBF.en.SAHYSMOD import sahysmodIO as IO
+import json
+import numpy as np
+
+print(IO.read_into_param_dic('D:\\Thesis\\pythonProject\\Tinamit\\tinamit\Calib\\459anew1.inp'))
+# p_dict = IO.read_into_param_dic('D:\\Thesis\\pythonProject\\Tinamit\\tinamit\Calib\\459anew1.inp')
 #
-# a = [[1, 2], [3, 4], [5, 6]]
-# a = np.asarray(a)
-# print(a.transpose())
-# with open('sensitivity.out', 'r+') as f:
-#     s = a.tolist()
-#     for p in s:
-#         print(p)
-#         f.write(json.dumps(p)+'\n')
+# with open('D:\\Thesis\\pythonProject\\Tinamit\\tinamit\Calib\\SA_algorithms\\poly_n4.inp', 'w') as f:
+#     n1 = p_dict['TO_N1'].tolist()
+#     n2 = p_dict['TO_N2'].tolist()
+#     n3 = p_dict['TO_N3'].tolist()
+#     n4 = p_dict['TO_N4'].tolist()
+#     #print(n1)
+#     poly_neighors = []
+#     for i, n in enumerate(n1):
+#         poly_neighors.append([n, n2[i], n3[i], n4[i]])
+#         f.write('{},\n'.format(json.dumps([n, n2[i], n3[i], n4[i]])))
+#         f.flush()
 # f.close()
+
