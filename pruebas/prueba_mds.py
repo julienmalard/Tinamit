ea66dbdf92690259d42a64beb8126d1f68d2641b
Julien Malard <julien.malard@mail.mcgill.ca>
2018-04-27 13:02:24 -0400

    Pruebas

---
+++ pruebas/prueba_mds.py
@@ -0,0 +1,9 @@
+import unittest
+
+from tinamit.EnvolturaMDS import generar_mds
+
+
+class PruebaLeerModelo(unittest.TestCase):
+
+    def prb_leer_mod(símimo):
+        símimo.assertEquals(1, 1)  # para hacer
