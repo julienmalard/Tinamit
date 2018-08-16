576d55349de04a8efcd3557df3835fd67b8aa564
Julien Malard <julien.malard@mail.mcgill.ca>
2018-08-16 15:18:19 -0400

    Error en prueba extensiones

--- pruebas/test_mds.py
+++ pruebas/test_mds.py
@@ -257,7 +257,7 @@ class Test_GenerarMDS(unittest.TestCase):
         """
 
         with símismo.assertRaises(ValueError):
-            generar_mds('Modelo con extensión no reconocida.வணக்கம்')
+            generar_mds(os.path.join(dir_act, 'recursos/MDS/Modelo con extensión no reconocida.வணக்கம்'))
 
     def test_generación_auto_mds_con_motor_especificado(símismo):
         """
@@ -276,7 +276,7 @@ class Test_GenerarMDS(unittest.TestCase):
         """
 
         with símismo.assertRaises(FileNotFoundError):
-            generar_mds('Yo no existo.mdl')
+            generar_mds('recursos/MDS/Yo no existo.mdl')
 
 
 def limpiar_mds(direc='./recursos/MDS'):
