d87e892ea79b2257062c7a4f934f07e3353608e3
Julien Malard <julien.malard@mail.mcgill.ca>
2018-08-27 17:31:47 -0400

    Calibraciones funcionan sin divergencias...por el momento.

--- tinamit/Análisis/sintaxis.py
+++ tinamit/Análisis/sintaxis.py
@@ -1,6 +1,7 @@
 import math as mat
 
 import numpy as np
+import regex
 from pkg_resources import resource_filename
 
 from lark import Lark, Tree
@@ -10,9 +11,8 @@ from tinamit.config import _
 
 try:
     import pymc3 as pm
-    import theano.tensor as T
 except ImportError:  # pragma: sin cobertura
-    pm = T = None
+    pm = None
 
 l_dialectos_potenciales = {
     'vensim': resource_filename('tinamit.Análisis', 'grams/gram_ecs.g'),
@@ -589,15 +589,14 @@ class Ecuación(object):
                     if líms[0] is líms[1] is None:
                         final = pm.Normal(name=nmbr, mu=0, sd=100 ** 2, shape=tmñ)
                     else:
-                        dist_norm = pm.Normal(name='transf' + nmbr, mu=0, sd=1, shape=tmñ)
+                        dist_norm = pm.Normal(name='transf' + nmbr, mu=0, sd=100 ** 2, shape=tmñ)
+                        exp = pm.math.exp(dist_norm)
                         if líms[0] is None:
-                            final = pm.Deterministic(nmbr, líms[1] - pm.math.exp(dist_norm))
+                            final = pm.Deterministic(nmbr, líms[1] - exp)
                         elif líms[1] is None:
-                            final = pm.Deterministic(nmbr, líms[0] + pm.math.exp(dist_norm))
+                            final = pm.Deterministic(nmbr, líms[0] + exp)
                         else:
-                            final = pm.Deterministic(
-                                nmbr, (pm.math.tanh(dist_norm) / 2 + 0.5) * (líms[1] - líms[0]) + líms[0]
-                            )
+                            final = pm.Deterministic(nmbr, exp / (1 + exp) * (líms[1] - líms[0]) + líms[0])
                         norm[p] = dist_norm
                 else:
                     dist, prms = aprioris[p]
@@ -625,13 +624,8 @@ class Ecuación(object):
 
             for p, líms in líms_paráms.items():
                 mu_v = dists_base[p]
-
-                if líms[0] is líms[1] is None:
-                    mu_sg = 2
-                else:
-                    mu_sg = 0.5
-
-                sg_v = pm.Gamma(name='sg_{}_nv_{}'.format(p, len(nv_jerarquía) - 1), mu=mu_sg, sd=0.2,
+                # sg_v = pm.HalfCauchy(name='sg_{}_nv_{}'.format(p, len(nv_jerarquía) - 1), beta=5, shape=(1,))  # type: pm.model.TransformedRV
+                sg_v = pm.Gamma(name='sg_{}_nv_{}'.format(p, len(nv_jerarquía) - 1), alpha=5, beta=1,
                                 shape=(1,))  # type: pm.model.TransformedRV
 
                 for í, nv in enumerate(nv_jerarquía[:-1]):
@@ -643,42 +637,42 @@ class Ecuación(object):
                     nmbr_sg = 'sg_{}_nv_{}'.format(p, í_nv)
                     nmbr_trsld = 'trlsd_{}_nv_{}'.format(p, í_nv)
 
-                    if False and (trsld_sub or últ_niv):
+                    if trsld_sub:
                         trsld = pm.Normal(name=nmbr_trsld, mu=0, sd=1, shape=tmñ_nv)
                         if líms[0] is líms[1] is None:
                             mu_v = pm.Deterministic(nmbr_mu, mu_v[nv] + trsld * sg_v[nv])
 
                         else:
                             mu_v = pm.Deterministic('transf' + nmbr_mu, mu_v[nv] + trsld * sg_v[nv])
+                            exp = pm.math.exp(mu_v)
                             if líms[0] is None:
-                                final = pm.Deterministic(nmbr_mu, líms[1] - pm.math.exp(mu_v))
+                                final = pm.Deterministic(nmbr_mu, líms[1] - exp)
                             elif líms[1] is None:
-                                final = pm.Deterministic(nmbr_mu, líms[0] + pm.math.exp(mu_v))
+                                final = pm.Deterministic(nmbr_mu, líms[0] + exp)
                             else:
-                                final = pm.Deterministic(
-                                    nmbr_mu, (pm.math.tanh(mu_v) / 2 + 0.5) * (líms[1] - líms[0]) + líms[0]
-                                )
+                                final = pm.Deterministic(nmbr_mu, exp / (1 + exp) * (líms[1] - líms[0]) + líms[0])
 
                             if últ_niv:
                                 mu_v = final
                     else:
                         if líms[0] is líms[1] is None:
                             mu_v = pm.Normal(name=nmbr_mu, mu=mu_v[nv], sd=sg_v[nv], shape=tmñ_nv)
+
                         else:
                             mu_v = pm.Normal(name='transf' + nmbr_mu, mu=mu_v[nv], sd=sg_v[nv], shape=tmñ_nv)
+                            exp = pm.math.exp(mu_v)
                             if líms[0] is None:
-                                final = pm.Deterministic(nmbr_mu, líms[1] - pm.math.exp(mu_v))
+                                final = pm.Deterministic(nmbr_mu, líms[1] - exp)
                             elif líms[1] is None:
-                                final = pm.Deterministic(nmbr_mu, líms[0] + pm.math.exp(mu_v))
+                                final = pm.Deterministic(nmbr_mu, líms[0] + exp)
                             else:
-                                final = pm.Deterministic(
-                                    nmbr_mu, (pm.math.tanh(mu_v) / 2 + 0.5) * (líms[1] - líms[0]) + líms[0]
-                                )
+                                final = pm.Deterministic(nmbr_mu, exp / (1 + exp) * (líms[1] - líms[0]) + líms[0])
+
                             if últ_niv:
                                 mu_v = final
 
                     if not últ_niv:
-                        sg_v = pm.Gamma(name=nmbr_sg, mu=mu_sg, sd=0.2, shape=tmñ_nv)
+                        sg_v = pm.Gamma(name=nmbr_sg, alpha=5, beta=1, shape=tmñ_nv)
 
                 egr[p] = mu_v
 
@@ -694,7 +688,7 @@ class Ecuación(object):
             mu = _VstrAPyMC3(
                 d_vars_pm=d_vars_pm, dialecto=símismo.dialecto, obs_x=obs_x, nv_jerarquía=nv_jerarquía,
             ).convertir(símismo.árbol)
-            sigma = pm.HalfNormal(name='sigma', sd=obs_y.values.std())
+            sigma = pm.HalfNormal(name='sigma', sd=max(1, (obs_y.max() - obs_y.min())))
 
             if binario:
                 # noinspection PyTypeChecker
@@ -750,9 +744,9 @@ _dic_funs = {
     'sinh': {'vensim': 'SINH', 'pm': pm.math.sinh if pm is not None else None, 'python': mat.sinh},
     'cosh': {'vensim': 'COSH', 'pm': pm.math.cosh if pm is not None else None, 'python': mat.cosh},
     'tanh': {'vensim': 'TANH', 'pm': pm.math.tanh if pm is not None else None, 'python': mat.tanh},
-    'asin': {'vensim': 'ARCSIN', 'pm': T.arcsin if T is not None else None, 'python': mat.asin},
-    'acos': {'vensim': 'ARCCOS', 'pm': T.arccos if T is not None else None, 'python': mat.acos},
-    'atan': {'vensim': 'ARCTAN', 'pm': T.arctan if T is not None else None, 'python': mat.atan},
+    'asin': {'vensim': 'ARCSIN', 'python': mat.asin},
+    'acos': {'vensim': 'ARCCOS', 'python': mat.acos},
+    'atan': {'vensim': 'ARCTAN', 'python': mat.atan},
     'si_sino': {'vensim': 'IF THEN ELSE', 'pm': pm.math.switch if pm is not None else None,
                 'python': lambda cond, si, sino: si if cond else sino}
 }
