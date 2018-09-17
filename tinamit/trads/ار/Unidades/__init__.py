3c909de4623537bbb7e0aa4059a3bee7d7f7f6c1
Julien Malard <julien.malard@mail.mcgill.ca>
2018-09-17 14:37:07 -0400

    Limpiar traducciones y establecer diretorio de pruebas para Appveyor

--- tinamit/trads/ار/Unidades/__init__.py
+++
@@ -1,6 +0,0 @@
-from tinamit.Unidades.__init__ import C_
-from tinamit.Unidades.__init__ import regu
-
-regu = regu
-
-C_ = C_
--- tinamit/trads/த/EnvolturaMDS/__init__.py
+++
@@ -1,4 +0,0 @@
-from tinamit.EnvolturaMDS.__init__ import dic_motores
-from tinamit.EnvolturaMDS.__init__ import generar_mds
-
-dic_motores = dic_motores
