f1f05475bb4d106c0d6be5bac63af169b0d8aaa9
julienmalard <julien.malard@mail.mcgill.ca>
2019-07-23 17:33:39 +0500

    Arreglos

--- setup.py
+++ setup.py
@@ -63,6 +63,5 @@ setup(
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
     ]
 )
