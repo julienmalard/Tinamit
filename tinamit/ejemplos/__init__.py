a6429b5487a39991049d4d96e197bc8efe0f4021
julienmalard <julien.malard@mail.mcgill.ca>
2019-07-24 08:22:55 +0500

    prueba nada más

--- tinamit/ejemplos/__init__.py
+++ tinamit/ejemplos/__init__.py
@@ -2,4 +2,4 @@ from pkg_resources import resource_filename
 
 
 def obt_ejemplo(arch):
-    return resource_filename('tinamit.ejemplos', arch)
+    return resource_filename('tinamit.ejemplos.sencillo', 'bf_bosques.py')
