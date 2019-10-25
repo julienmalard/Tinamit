e9c8fcd847c1a1587adaee3dbb431b3cdc2dfd0f
julienmalard <julien.malard@mail.mcgill.ca>
2019-10-25 14:20:38 -0400

    Cambiocitos

--- tinamit/envolt/mba/mesa/_auto.py
+++ tinamit/envolt/mba/mesa/_auto.py
@@ -1,17 +1,16 @@
-from mesa import Model
 from tinamit.config import _
-
+from ._envolt import EnvoltModeloMesa
 from ...utils import _importar_de_arch_python
 
 
 def _gen_modelo(mod):
-    if isinstance(mod, Model):
+    if isinstance(mod, EnvoltModeloMesa):
         raise TypeError('`mod` debe ser una clase de `mesa.Model`, y no una instancia.')
-    elif callable(mod) and issubclass(mod, Model):
+    elif callable(mod) and issubclass(mod, EnvoltModeloMesa):
         return mod
     elif isinstance(mod, str):
-        return _importar_de_arch_python(mod, clase=Model, nombre='Modelo', instanciar=False)
+        return _importar_de_arch_python(mod, clase=EnvoltModeloMesa, nombre='Modelo', instanciar=False)
     raise ValueError(
-        _('`modelo` debe ser un modelo mesa.Model, o un archivo '
-          'Python que contiene uno, no {tipo}.').format(tipo=type(mod))
+        _('`modelo` debe ser una subclase de tinamit.envolt.mba.mesa.EnvoltModeloMesa, o un archivo '
+          'Python que contiene una, no "{mod}".').format(mod=mod)
     )
